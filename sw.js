2802d49a5eb9b8d9294ff47017278fde02b54f4f
JosueRauda7 <katerozarack@gmail.com>
2021-02-22 17:15:38 -0600

    Modificaciones de iPhoneX

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ if (url.includes("localhost")) scriptLocation = "js/sw-utils.js";
 importScripts(scriptLocation);
 
 // Constantes de nombres y versionamiento de caches
-const STATIC_CACHE = "static-v2";
+const STATIC_CACHE = "static-v3";
 const INMUTABLE_CACHE = "inmutable-v1";
-const DYNAMIC_CACHE = "dynamic-v1";
+const DYNAMIC_CACHE = "dynamic-v2";
 
 // Constantes para app_shell statico e inmutable
 const APP_SHELL = [
