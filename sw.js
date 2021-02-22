363ba288e6daa7f3d248820347a0be2206e191a2
JosueRauda7 <katerozarack@gmail.com>
2021-02-22 17:04:57 -0600

    Aplicando cambios

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ if (url.includes("localhost")) scriptLocation = "js/sw-utils.js";
 importScripts(scriptLocation);
 
 // Constantes de nombres y versionamiento de caches
-const STATIC_CACHE = "static-v1";
+const STATIC_CACHE = "static-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 const DYNAMIC_CACHE = "dynamic-v1";
 
